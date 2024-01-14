1be584a77b57d8e5304ed3f6117a906a5fd28f4d
Alyque Farishta <alyquefarishta@n3-108-87.dhcp.drexel.edu>
2024-01-14 12:38:12 -0500

    fixed naming

--- queries/db.js
+++ queries/db.js
@@ -9,7 +9,7 @@ const pool = new Pool(process.env.NODE_ENV === 'production' ? {
 } : {
     user: 'postgres',
     host: 'localhost',
-    database: 'RestaurantApp-Api',
+    database: 'restaurantapp',
     password: '',
     port: 5432,
 });
