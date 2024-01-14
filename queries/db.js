92896d054d0f96aa71c30786a46b9a0f0854823e
su83 <su83@drexel.edu>
2024-01-14 12:37:43 -0500

    fixed psql file

--- queries/db.js
+++ queries/db.js
@@ -9,7 +9,7 @@ const pool = new Pool(process.env.NODE_ENV === 'production' ? {
 } : {
     user: 'postgres',
     host: 'localhost',
-    database: 'RestaurantApp-Api',
+    database: 'RestaurantApp',
     password: '',
     port: 5432,
 });
