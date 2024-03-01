984e9ad0ceac4878e363277d40b3804bc8ef0dc0
su83 <su83@drexel.edu>
2024-03-01 15:22:46 -0500

    added db health checker endpoint

--- routers/api.js
+++ routers/api.js
@@ -16,6 +16,14 @@ const getTargetResource = require('../middleware/getTargetResource');
 
 const router = Router();
 
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+
 router.get('/', requireAuth, (req, res) => {
     res.status(200).send(req.user.id);
 });
