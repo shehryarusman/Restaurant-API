a05a9952ad07b4e83edc29a68b7cf1b4bca2f40c
John Khachian <jaksawesome10@gmail.com>
2024-03-08 15:36:13 -0500

    updated database to use AWS

--- queries/db.js
+++ queries/db.js
@@ -1,17 +1,11 @@
 const { Pool } = require('pg');
 
-const pool = new Pool(process.env.NODE_ENV === 'production' ? {
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-} : {
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: 5432,
+const pool = new Pool({
+    user: process.env.RDS_USERNAME,
+    host: process.env.RDS_HOSTNAME,
+    database: process.env.RDS_DB_NAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_DB_PORT || 5432,
 });
 
 module.exports = pool;
